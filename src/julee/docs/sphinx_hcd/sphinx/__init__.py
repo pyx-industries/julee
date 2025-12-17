dc88d246da100cf915bd0beaf14354aba0719377
Chris Gough <chris.gough@gosource.com.au>
2025-12-18 09:10:58 +1100

    create HCDCcontext manager, initialization for sphinx integration

--- src/julee/docs/sphinx_hcd/sphinx/__init__.py
+++ src/julee/docs/sphinx_hcd/sphinx/__init__.py
@@ -3,10 +3,26 @@
 Contains Sphinx-specific code:
 - adapters.py: SyncRepositoryAdapter for sync access to async repos
 - context.py: HCDContext for unified repository access
+- initialization.py: Builder-inited handlers
 - directives/: Sphinx directive implementations
 - event_handlers/: Sphinx lifecycle event handlers
 """
 
 from .adapters import SyncRepositoryAdapter
+from .context import (
+    HCDContext,
+    ensure_hcd_context,
+    get_hcd_context,
+    set_hcd_context,
+)
+from .initialization import initialize_hcd_context, purge_doc_from_context
 
-__all__ = ["SyncRepositoryAdapter"]
+__all__ = [
+    "HCDContext",
+    "SyncRepositoryAdapter",
+    "ensure_hcd_context",
+    "get_hcd_context",
+    "initialize_hcd_context",
+    "purge_doc_from_context",
+    "set_hcd_context",
+]
